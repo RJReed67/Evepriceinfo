0bc14120fb35f93bfb121a29c2905429e4ea783e
Ron Reed <rjreed67@gmail.com>
2014-02-24 09:34:59 -0600

    Updated weekly code for giveaway logging.

--- Evepriceinfo.pl
+++ Evepriceinfo.pl
@@ -544,7 +544,7 @@ sub irc_botcmd_wg500 {
                        $winner2 = $winner." (not following)";
                     }
                     $sth->finish;
-                    $sth = $dbh->prepare('INSERT INTO giveaway SET GiveTitle=?, Threshold=?, AutoGive=?, StartDate=NOW(), EndDate=NOW(), Winner=?');
+                    $sth = $dbh->prepare('INSERT INTO giveaway SET GiveTitle=?, Threshold=500, AutoGive=?, StartDate=NOW(), EndDate=NOW(), Winner=?');
                     $sth->execute("Weekly Giveaway",0,$winner);
                     $sth->finish;
                     $irc->yield(privmsg => $where, "/me Congratulations $winner2, you've won this week's giveaway!");
